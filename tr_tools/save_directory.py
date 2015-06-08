78cb3e7c6b44b7812743a05e8cbe2e806bb4591f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-06-08 19:35:44 +0200

    save_directory: save result UTF-8 encoded

--- tr_tools/save_directory.py
+++ tr_tools/save_directory.py
@@ -1,10 +1,17 @@
 import json
+import sys
 
 def save_directory(path, data):
     for lang, trans in data.items():
-        json.dump(
-            trans,
-            open(path + '/' + lang + '.json', 'w'),
-            indent=4,
-            sort_keys=True
-        )
+        result = json.dumps(
+                trans,
+                indent=4,
+                ensure_ascii=False,
+                sort_keys=True
+            )
+
+        # compatibility Python2
+        if sys.version_info < (3, 0):
+            result = result.encode('UTF-8')
+
+        open(path + '/' + lang + '.json', 'w').write(result)
